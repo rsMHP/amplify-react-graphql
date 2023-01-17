d7452ee4d30cc5b1e44aae589ba00f116abe9736
Robert Saalbach <robert.saalbach@mhp.com>
2023-01-17 08:04:41 +0100

    add auth

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
+import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
+import { Amplify } from 'aws-amplify';
+import { withAuthenticator } from '@aws-amplify/ui-react';
+import '@aws-amplify/ui-react/styles.css';
+import awsExports from './aws-exports';
+Amplify.configure(awsExports);
+
+function App({ signOut, user }) {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        Meine erste bei AWS gehostete App
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <h1>Hello {user.username}</h1>
+      <button onClick={signOut}>Sign out</button>
+    </>
   );
 }
 
-export default App;
+export default withAuthenticator(App);
